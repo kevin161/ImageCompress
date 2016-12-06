122d13320cb9c030500e52e12302a38b6b452dd0
kevin161 <kevin161@sina.com>
2016-12-06 15:35:20 +0800

    尺寸压缩、质量压缩、采样率压缩、哈夫曼算法压缩

---
+++ obj/local/armeabi-v7a/objs/bitherjni/bitherlibjni.o.d
@@ -0,0 +1,150 @@
+obj/local/armeabi-v7a/objs/bitherjni/bitherlibjni.o: jni/bitherlibjni.cpp \
+ jni/bitherlibjni.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/jni.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/string.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/malloc.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/bitmap.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdint.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/log.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdio.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/setjmp.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/setjmp.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/math.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/limits.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/time.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/jpeg/jpeglib.h jni/jpeg/android/jconfig.h jni/jpeg/jmorecfg.h \
+ jni/jpeg/cdjpeg.h jni/jpeg/jinclude.h jni/jpeg/jconfig.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/alloca.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/strings.h \
+ E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/memory.h \
+ jni/jpeg/jpeglib.h jni/jpeg/jerror.h jni/jpeg/cderror.h \
+ jni/jpeg/jversion.h jni/jpeg/android/config.h
+
+jni/bitherlibjni.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/jni.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/string.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/bitmap.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/setjmp.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/setjmp.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/math.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/limits.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/time.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/jpeg/jpeglib.h:
+
+jni/jpeg/android/jconfig.h:
+
+jni/jpeg/jmorecfg.h:
+
+jni/jpeg/cdjpeg.h:
+
+jni/jpeg/jinclude.h:
+
+jni/jpeg/jconfig.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/strings.h:
+
+E:/NDK/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/memory.h:
+
+jni/jpeg/jpeglib.h:
+
+jni/jpeg/jerror.h:
+
+jni/jpeg/cderror.h:
+
+jni/jpeg/jversion.h:
+
+jni/jpeg/android/config.h:
